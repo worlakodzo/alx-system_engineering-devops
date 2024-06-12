b15f6eba96c62d15d7bcc30b361fdd7f6d06ec0d
Worlako Dzokoto <worlaco.dzo@gmail.com>
2024-06-12 22:31:07 +0000

    added 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# SSH into the server
+
+file { '~/.ssh/config':
+  ensure => present,
+  mode   => '0600',
+  content => "
+Host school
+    HostName 54.209.217.152
+    User your_username
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+",
+}
